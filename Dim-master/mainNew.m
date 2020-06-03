26168336801fc66d8328e08a27ad17c97163fc1d
zhan2296 <jiahzhang2@outlook.com>
2020-06-03 12:11:46 -0400

    2020.6.3

--- Dim-master/mainNew.m
+++ Dim-master/mainNew.m
@@ -10,13 +10,13 @@ set(0,'defaulttextinterpreter','latex')
 global ModelInfo
 
 %% Setup
-N_L = 100;
+N_L = 150;
 N_H_start = 5;
 N_H = N_H_start;
 D = 5;
 
 I = 1;
-I_max = 4;
+I_max = 5;
 O = cell(1,I_max+1);
 count = 0;
 points_add = zeros(1,I_max);
@@ -24,7 +24,7 @@ process_error = zeros(1,I_max);
 
 % lb = zeros(1,D);
 % ub = ones(1,D);
-jitter = 1e-9;
+jitter = 1e-7;
 noise_L = 0;
 noise_H = 0;
 ModelInfo.jitter=jitter;
@@ -40,6 +40,7 @@ ModelInfo.y_H = ModelInfo.y_H + noise_H*randn(N_H,1);
 ModelInfo.X_L = randn(N_L, D);
 ModelInfo.y_L = f_L(ModelInfo.X_L);
 ModelInfo.y_L = ModelInfo.y_L + noise_L*randn(N_L,1);
+f_H_pool = f_H(ModelInfo.X_L);
 
 n_test = 50;
 X_T = randn(n_test, D);
@@ -47,6 +48,9 @@ y_exact = f_H(X_T);
 
 n_SIR = 1000;
 X_SIR = randn(n_SIR, D);
+
+X_ORIGIN = X_SIR;
+low_origin = f_L(X_SIR);
 
 %% First rotation
 A1 = my_SIR(5, 50, ModelInfo.X_L', ModelInfo.y_L);
@@ -77,14 +81,13 @@ options = optimoptions('fminunc','GradObj','on','Display','iter',...
 %% Active Learning for adding new f_H
 relative_error = 1;
 N_iteration = 1;
-while  N_iteration < 10
+while  N_iteration < 5
 
     index = find(var_f == max(var_f),1,'first');
     X_H_new = ModelInfo.X_L(index,:);
     N_H = N_H + 1;
     ModelInfo.X_H = vertcat(ModelInfo.X_H, X_H_new);
-    ModelInfo.y_H = f_H(ModelInfo.X_H);
-    ModelInfo.y_H = ModelInfo.y_H + noise_H*randn(N_H,1);
+    ModelInfo.y_H = vertcat(ModelInfo.y_H, f_H_pool(index));
     
     hyp = [log(ones(1, 2*D+2)) 1 -4 -4];
     options = optimoptions('fminunc','GradObj','on','Display','iter',...
@@ -160,15 +163,14 @@ while I <= I_max
         
         relative_error = 1;
         N_iteration = 1;
-        while  N_iteration < 10
+        while  N_iteration < 5
 
             index = find(var_f == max(var_f),1,'first');
             X_H_new = ModelInfo.X_L(index,:);
             N_H = N_H + 1;
             ModelInfo.X_H = vertcat(ModelInfo.X_H, X_H_new);
-            ModelInfo.y_H = f_H(ModelInfo.X_H);
-            ModelInfo.y_H = ModelInfo.y_H + noise_H*randn(N_H,1);
-
+            ModelInfo.y_H = vertcat(ModelInfo.y_H, f_H_pool(index));
+            
             hyp = [log(ones(1, 2*D+2)) 1 -4 -4];
             options = optimoptions('fminunc','GradObj','on','Display','iter',...
             'Algorithm','trust-region','Diagnostics','on','DerivativeCheck','on',...
@@ -210,6 +212,9 @@ while I <= I_max
     end   
 end
 
+Sample_h = mean_f_SIR - low_origin ;
+A_h = my_SIR(5, 1000, X_ORIGIN', Sample_h);
+
 
 %% Post-processing
-rmpath ./Utilities
+                                    rmpath ./Utilities
